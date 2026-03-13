6238ccd5ff8fa90e2d3715b0d5e8a67da7cbfd93
Lk7pee <gualbertofilipe13@gmail.com>
2026-03-13 19:24:00 -0300

    Conecta validação de CEP, API ViaCEP e cálculo de frete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 const readline = require("readline");
+const { isValidCep } = require("./utils/cepValidator");
+const { buscarEnderecoPorCep } = require("./services/viacepService");
+const { calcularFrete } = require("./rules/freightRules");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question("Digite o CEP: ", (cep) => {
-  console.log("CEP recebido:", cep);
-  rl.close();
+rl.question("Digite o CEP: ", async (cep) => {
+  try {
+
+    if (!isValidCep(cep)) {
+      throw new Error("CEP inválido.");
+    }
+
+    const endereco = await buscarEnderecoPorCep(cep);
+
+    const resultadoFrete = calcularFrete(endereco);
+
+    console.log("\nEndereço encontrado:");
+    console.log(`Cidade: ${endereco.localidade}`);
+    console.log(`Estado: ${endereco.uf}`);
+
+    console.log("\nResultado do frete:");
+    console.log(resultadoFrete.mensagem);
+
+    if (resultadoFrete.valorFrete !== null) {
+      console.log(`Valor do frete: R$ ${resultadoFrete.valorFrete.toFixed(2)}`);
+    }
+
+  } catch (error) {
+    console.log("Erro:", error.message);
+  } finally {
+    rl.close();
+  }
 });
