fc545604fde4e9c4c8159db19b2414b1451e4fcc
Lk7pee <gualbertofilipe13@gmail.com>
2026-03-13 19:22:54 -0300

    Integração com API ViaCEP

--- src/services/viacepService.js
+++ src/services/viacepService.js
@@ -1,1 +1,19 @@
+async function buscarEnderecoPorCep(cep) {
+  const cepLimpo = cep.replace(/\D/g, "");
 
+  const response = await fetch(`https://viacep.com.br/ws/${cepLimpo}/json/`);
+
+  if (!response.ok) {
+    throw new Error("Erro ao consultar a API ViaCEP.");
+  }
+
+  const data = await response.json();
+
+  if (data.erro) {
+    throw new Error("CEP não encontrado.");
+  }
+
+  return data;
+}
+
+module.exports = { buscarEnderecoPorCep };
